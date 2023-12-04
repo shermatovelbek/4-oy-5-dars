5d608ffa00bf6fedc68ee1fc379fad6f8be4366f
shermatovelbek <shermatovelbek772@gmail.com>
2023-12-04 21:15:38 +0500

    Uyga vazifa tugatildi

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//1-misol
+
+// let n = +prompt ("biror bir sonni kiriting ...");
+// let sum = 0 ;
+// for( let i = 1; i <= n; i++ ){
+//     if(n % 2 == 1){
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+
+//2-misol
+// let n = +prompt("son kiriting");
+// let sum = 1;
+// for ( let i = n; i >= 1 ; i--){
+//     console.log(i)
+//     if(i % 3 == 0 ){
+//         sum *= i
+//         }
+// }
+// console.log(sum);4
+
+
+//3-misol
+
+let a = 1;
+let b = 10;
+let sum = 1;
+
+for(let i = a; i <= b; i += 3){
+    sum *= 1 
+}
+
+console.log(sum)
+
